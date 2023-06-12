29a1c55584bf363552d0d6adaf7c71ade7481d51
MauricioJMoralesV <129233232+MauricioJMoralesV@users.noreply.github.com>
2023-06-12 00:42:07 -0400

    Sprint modulo 4 ok

---
+++ src/Administrativo.java
@@ -0,0 +1,61 @@
+class Administrativo extends Usuario {
+    private String area;
+    private String experienciaPrevia;
+
+    public Administrativo() {
+    }
+
+    public Administrativo(String nombre, String fechaNacimiento, int run, String area, String experienciaPrevia) {
+        super(nombre, fechaNacimiento, run);
+        this.area = area;
+        this.experienciaPrevia = experienciaPrevia;
+    }
+
+    //Accesadores y mutadores
+    public String getArea() {
+        return area;
+    }
+
+    public String getExperienciaPrevia() {
+        return experienciaPrevia;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public void setExperienciaPrevia(String experienciaPrevia) {
+        this.experienciaPrevia = experienciaPrevia;
+    }
+
+    //Métodos personalizados
+    @Override
+    public String toString() {
+        return super.toString() + "\nÁrea: " + area + "\nExperiencia Previa: " + experienciaPrevia;
+    }
+
+    //Validadores
+//Area
+    public static boolean validarAreaVacia(String area) {
+        return area != null && !area.isEmpty();
+    }
+
+    public static boolean validarArea(String area) {
+        if (area == null || area.length() < 5 || area.length() > 20) {
+            return false;
+        }
+        return true;
+    }
+
+    //Experiencia Previa
+    public static boolean validarExperienciaPrevia(String experienciaPrevia) {
+        if (experienciaPrevia == null || experienciaPrevia.length() > 100) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean validarExperienciaPreviaVacio(String experienciaPrevia) {
+        return experienciaPrevia != null && !experienciaPrevia.isEmpty();
+    }
+}
